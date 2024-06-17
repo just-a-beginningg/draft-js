b2d6af3c554b0b42481af42a23c3cddb508009e0
Arun <youngDeveloper37@gmail.com>
2024-06-17 09:33:39 +0530

    FIX :: Code refactoring

--- src/hooks/useTextContent.ts
+++ src/hooks/useTextContent.ts
@@ -11,6 +11,7 @@ export const useTextContent = ({ children, regExp }: UseTextContentProps) => {
     if (!children || children.length == 0) {
       return;
     }
+
     const text = children[0].props.text ?? "";
     setContent(text.replace(regExp, ""));
   }, [children, regExp]);
